8a6c5317f9b25ca9f5de9bfc1efcde1b8b222a18
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-27 01:32:51 -0300

    Adiciona parcialmente os arquivos dos exercicios do dia 4.3

---
+++ exercises/4.3/asteriscos-quadrado.js
@@ -0,0 +1,11 @@
+let n = 5;
+let texto = '*';
+let escreve = '';
+
+for (let fator = 0; fator < 5; fator += 1) {
+    escreve += texto;
+}
+
+for (let fator = 0; fator < 5; fator += 1) {
+    console.log(escreve);
+}
