7e6f7c80b196a29607fb8ee1cda4e5a514e462c1
tiagoornelas <advtiagoornelas@gmail.com>
2021-05-19 20:02:13 -0300

    Adiciona grande parte dos exercicios de 7.1

--- exercises/7.1/script.js
+++ exercises/7.1/script.js
@@ -1,3 +1,5 @@
+// Parte I
+
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
@@ -7,7 +9,61 @@ const testingScope = (escopo) => {
     let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
     console.log(elseScope);
   }
-  console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
 };
 
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const orderByNb = (array) => {
+  // For para verificar qual é o menor de todos.
+  // Depois pega esse menor, adiciona na ordenada e tira do resto
+  let ordened = [];
+  let lowestNumber;
+  for (let number = 0; number < 6; number += 1) {
+    for (let index = 1; index < array.length; index += 1) {
+      if (array[index - 1] > array[index]) {
+        lowestNumber = array[index - 1];
+        array[index - 1] = array[index];
+        array[index] = lowestNumber;
+      }
+    }
+  }
+  console.log(
+    `Os números ${array.join(', ')} se encontram ordenados de forma crescente!`
+  );
+};
+
+orderByNb(oddsAndEvens);
+
+oddsAndEvens.sort((a, b) => a - b);
+console.log(oddsAndEvens);
+
+// Parte II
+
+// Exercicio 1
+
+const fatorial = (number) => {
+  let result = number;
+  for (let index = number - 1; index > 0; index -= 1) {
+    result *= index;
+  }
+  console.log(result);
+};
+
+fatorial(4);
+
+//Exercicio 2
+
+const longestWord = (phrase) => {
+  let words = phrase.split(' ');
+  let bigOne = words[0];
+  for (let index = 1; index < words.length; index += 1) {
+    if (words[index].length > bigOne.length) {
+      bigOne = words[index];
+    }
+  }
+  console.log(bigOne);
+};
+
+longestWord('Antônio foi no banheiro e não sabemos o que aconteceu');
