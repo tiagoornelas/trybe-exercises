e25cdf10400f8d1745b22b1e9c9ed4f7fca4141d
tiagoornelas <advtiagoornelas@gmail.com>
2021-05-25 15:45:54 -0300

    Adiciona pasta do dia 8.1 e exercicios pre live

---
+++ exercises/8.1/prelive.js
@@ -0,0 +1,7 @@
+const wakeUp = () => 'Acordando!!';
+const coffeeTime = () => 'Bora tomar café!!';
+const napTime = () => 'Partiu dormir!!';
+
+const doingThings = (funct) => console.log(funct);
+
+doingThings(wakeUp());
