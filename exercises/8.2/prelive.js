1aa2da6ad08e1614019a4045107449f973e98f59
tiagoornelas <advtiagoornelas@gmail.com>
2021-05-26 15:18:28 -0300

    Resolve os exercicios prelive do dia 8.2

--- exercises/8.2/prelive.js
+++ exercises/8.2/prelive.js
@@ -10,3 +10,81 @@ const showEmailList = (email) => {
 };
 
 emailListInData.forEach(showEmailList);
+
+// 2
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+};
+
+console.log(findDivisibleBy3And5());
+
+// 3
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+function findMusic(id) {
+  return musicas.find((music) => music.id === id);
+}
+
+console.log(findMusic('31031685'));
+
+// 4
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return names.some((item) => item === name);
+};
+
+console.log(hasName(names, 'Ana'));
+
+// 5
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((person) => person.age >= minimumAge);
+};
+
+console.log(verifyAges(people, 18));
+
+// 6
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => a.age > b.age);
+
+console.log(people);
+
+// 7
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a, b) => a.age < b.age);
+
+console.log(people);
