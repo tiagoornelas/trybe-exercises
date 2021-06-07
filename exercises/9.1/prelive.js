2db3d238c8a01d65d144b456667da1e862891ec0
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-07 15:53:49 -0300

    Adiciona a pasta do dia 9.1 e exercicios pre e pos live

---
+++ exercises/9.1/prelive.js
@@ -0,0 +1,126 @@
+setTimeout(() => {
+  console.log('Comprar parafusos'); // Comprar parafusos
+  console.log('Adicionar ao estoque'); // Adicionar ao estoque
+}, 2000);
+
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]);
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => {
+  list.push(number);
+  console.log(list);
+};
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+
+// EXERCICIO 1
+
+const assert = require('assert');
+
+const userFullName = ({ firstName, lastName }) =>
+  `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) =>
+  `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: 'Ivan',
+    lastName: 'Ivanovich',
+    nationality: 'Russian',
+  };
+  return callback(userToReturn);
+};
+
+assert.strictEqual(getUser(userFullName), 'Hello! My name is Ivan Ivanovich'); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), 'Ivan is Russian'); // complete a chamada da função de getUser
+
+// EXERCICIO 2
+
+const userFullName = ({ firstName, lastName }) =>
+  `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) =>
+  `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) =>
+  Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: 'Ivan',
+      lastName: 'Ivanovich',
+      nationality: 'Russian',
+    };
+    console.log(callback(user));
+  }, delay());
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// EXERCICIO 3
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) =>
+  console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) =>
+  Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) =>
+  console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onFailure) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if (didOperationSucceed) {
+      const country = {
+        name: 'Brazil',
+        hdi: 0.759,
+        currency: 'Real',
+      };
+      onSuccess(country);
+    } else {
+      const errorMessage = 'Country could not be found';
+      onFailure(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
